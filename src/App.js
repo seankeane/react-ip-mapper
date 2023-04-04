a60b969efb510bf5c1df396c7e004c7e63ab2eb5
Sean Keane <seankeane@live.ie>
2023-04-04 18:14:47 +0100

    Switching to beta to support marker collision

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const loadGoogleMapScript = (callback) => {
         callback();
     } else {
         const googleMapScript = document.createElement("script");
-        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&libraries=geometry`;
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?v=beta&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&libraries=geometry`;
         window.document.body.appendChild(googleMapScript);
         googleMapScript.addEventListener("load", callback);
     }
