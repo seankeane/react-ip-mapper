cf993eda5c5a9ca776025cc23cdd6c9b591dd766
Sean Keane <seankeane@live.ie>
2023-04-04 23:11:27 +0100

    Error handling for APILayer call

--- src/IPLoc.js
+++ src/IPLoc.js
@@ -8,9 +8,15 @@ const IPLoc = async (ip) => {
         headers: myHeaders
     };
 
-    const response = await fetch(`https://api.apilayer.com/ip_to_location/${ip}`, requestOptions);
-
-    return response.json();
+    try {
+        const response = await fetch(`https://api.apilayer.com/ip_to_location/${ip}`, requestOptions);
+        if (!response.ok) {
+            throw new Error("Bad response from server:" + response);
+        }
+        return response.json();
+    } catch (error) {
+        console.error("There has been a problem with your fetch operation:", error);
+    }
 }
 
 export default IPLoc;
